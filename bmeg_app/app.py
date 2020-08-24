cfe0352b568a93a625665ea3d2d03b2e9d41e2ee
Jordan Lee <leejor@ohsu.edu>
2020-08-24 15:28:23 -0700

    clean up

--- bmeg_app/app.py
+++ bmeg_app/app.py
@@ -1,12 +1,7 @@
 import flask
 import dash
 import dash_bootstrap_components as dbc
-import dash_html_components as html
 
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 server = flask.Flask(__name__)
-
-# No docker, local 
 app = dash.Dash(__name__, server=server, url_base_pathname = '/',external_stylesheets=[dbc.themes.BOOTSTRAP])
-# app = dash.Dash(__name__, server=server, url_base_pathname = '/', external_stylesheets=external_stylesheets)
 app.config.suppress_callback_exceptions=True
