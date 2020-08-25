abc22bf5b59f584f9b0b53aac928957f90679317
Jordan Lee <leejor@ohsu.edu>
2020-08-25 11:18:20 -0700

    sidebar

--- bmeg_app/__main__.py
+++ bmeg_app/__main__.py
@@ -1,53 +1,144 @@
 from bmeg_app.app import app
-import bmeg_app.appLayout as ly
 from bmeg_app.views import app_about, app_tcga, app_drugresp, app_repurpose_drug
 import base64
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-from dash.dependencies import Input, Output
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output, State
+# https://github.com/facultyai/dash-bootstrap-components/tree/master/examples/multi-page-apps/responsive-collapsible-sidebar
 
-#######
-# Prep
-#######
-# Main color scheme
-main_colors=ly.main_colors
-styles=ly.styles
-tab_style=ly.styles['tab_style']
 
-# Images
-image_filename = 'bmeg_app/images/bmeg_logo.png' 
-encoded_image3 = base64.b64encode(open(image_filename, 'rb').read())
- 
-#######
-# Page  
-####### 
-app.layout = html.Div([
-    dcc.Location(id='url'),
-    dcc.Tabs(id='main-tab', value='tab-Repurp', children=[
-        dcc.Tab(label='Drug Repurposing', value='tab-Repurp',style=tab_style, selected_style=tab_style),
-        dcc.Tab(label='TCGA Clustering', value='tab-Tcga',style=tab_style, selected_style=tab_style),
-        dcc.Tab(label='Literature Drug Responses', value='tab-DrugResponse',style=tab_style, selected_style=tab_style),
-        dcc.Tab(label='About', value='tab-About', style=tab_style, selected_style=tab_style),
-    ]),
-    html.Div(html.Img(src='data:image/png;base64,{}'.format(encoded_image3.decode()),
-        style={'height':'15%','width':'15%', 'marginTop': 0, 'marginBottom':0}),
-        style={'textAlign':'center'}),
-    html.Div(id='main-tab-content'),
-])
+# we use the Row and Col components to construct the sidebar header
+# it consists of a title, and a toggle, the latter is hidden on large screens
+sidebar_header = dbc.Row(
+    [
+        dbc.Col(html.H4("BMEG", className="display-4")),
+        dbc.Col(
+            [
+                html.Button(
+                    # use the Bootstrap navbar-toggler classes to style
+                    html.Span(className="navbar-toggler-icon"),
+                    className="navbar-toggler",
+                    # the navbar-toggler classes don't set color
+                    style={
+                        "color": "rgba(0,0,0,.5)",
+                        "border-color": "rgba(0,0,0,.1)",
+                    },
+                    id="navbar-toggle",
+                ),
+                html.Button(
+                    # use the Bootstrap navbar-toggler classes to style
+                    html.Span(className="navbar-toggler-icon"),
+                    className="navbar-toggler",
+                    # the navbar-toggler classes don't set color
+                    style={
+                        "color": "rgba(0,0,0,.5)",
+                        "border-color": "rgba(0,0,0,.1)",
+                    },
+                    id="sidebar-toggle",
+                ),
+            ],
+            # the column containing the toggle will be only as wide as the
+            # toggle, resulting in the toggle being right aligned
+            width="auto",
+            # vertically align the toggle in the center
+            align="center",
+        ),
+    ]
+)
 
-@app.callback(Output('main-tab-content', 'children'),
-              [Input('main-tab', 'value')])
-def render_content(tab):
-    if tab == 'tab-DrugResponse':
-        return html.Div([app_drugresp.tab_layout]) 
-    elif tab == 'tab-Tcga':
-        return html.Div([app_tcga.tab_layout])    
-    elif tab == 'tab-Repurp':
+sidebar = html.Div(
+    [
+        sidebar_header,
+        # we wrap the horizontal rule and short blurb in a div that can be
+        # hidden on a small screen
+        html.Div(
+            [
+                html.Hr(),
+                html.P(
+                    "A graph database for "
+                    "merging and analyzing connected data",
+                    className="lead",
+                ),
+            ],
+            id="blurb",
+        ),
+        # use the Collapse component to animate hiding / revealing links
+        dbc.Collapse(
+            dbc.Nav(
+                [
+                    dbc.NavLink("Drug Repurposing", href="/page-1", id="page-1-link"),
+                    dbc.NavLink("TCGA Clustering", href="/page-2", id="page-2-link"),
+                    dbc.NavLink("Literature Drug Responses", href="/page-3", id="page-3-link"),
+                ],
+                vertical=True,
+                pills=True,
+            ),
+            id="collapse",
+        ),
+    ],
+    id="sidebar",
+)
+
+content = html.Div(id="page-content")
+
+app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+
+
+# this callback uses the current pathname to set the active state of the
+# corresponding nav link to true, allowing users to tell see page they are on
+@app.callback(
+    [Output(f"page-{i}-link", "active") for i in range(1, 4)],
+    [Input("url", "pathname")],
+)
+def toggle_active_links(pathname):
+    if pathname == "/":
+        # Treat Drug Repurposing as the homepage / index
+        return True, False, False
+    return [pathname == f"/page-{i}" for i in range(1, 4)]
+
+
+@app.callback(Output("page-content", "children"), 
+    [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname in ["/", "/page-1"]:
         return html.Div([app_repurpose_drug.tab_layout]) 
-    elif tab == 'tab-About':
-        return html.Div([app_about.tab_layout])
-        
-        
+    elif pathname == "/page-2":
+        return html.Div([app_tcga.tab_layout])    
+    elif pathname == "/page-3":
+        return html.Div([app_drugresp.tab_layout]) 
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
+
+
+@app.callback(
+    Output("sidebar", "className"),
+    [Input("sidebar-toggle", "n_clicks")],
+    [State("sidebar", "className")],
+)
+def toggle_classname(n, classname):
+    if n and classname == "":
+        return "collapsed"
+    return ""
+
+
+@app.callback(
+    Output("collapse", "is_open"),
+    [Input("navbar-toggle", "n_clicks")],
+    [State("collapse", "is_open")],
+)
+def toggle_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
+
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', debug=True, port=80)
