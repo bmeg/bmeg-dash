7f3d887cf6bae3a79486219ea314bb5d5a1e9fb9
Brian Walsh <walsbr@ohsu.edu>
2020-09-17 12:42:22 -0700

    Add elastic etl

---
+++ autocomplete/app/data/setup-elastic.sh
@@ -0,0 +1,37 @@
+# delete and re-create index
+echo '>dropping index'
+curl -X DELETE $ELASTICSEARCH_URL/genes
+
+echo
+echo '>configuring index'
+curl -X PUT $ELASTICSEARCH_URL/genes -H "Content-Type: application/json" -d'
+{
+  "settings": {
+    "analysis": {
+      "analyzer": {
+        "ensembl_analyzer": {
+          "tokenizer": "ensembl_tokenizer"
+        }
+      },
+      "tokenizer": {
+        "ensembl_tokenizer": {
+          "type": "simple_pattern_split",
+          "pattern": "ENSG0*"
+        }
+      }
+    }
+  },
+  "mappings": {
+    "properties": {
+      "symbol": {
+        "type": "search_as_you_type"
+      },
+      "ensemble_id": {
+        "type": "search_as_you_type",
+        "analyzer":"ensembl_analyzer"
+      }      
+    }
+  }
+}'
+echo
+echo '>Ready'
