64d13fb5eba9ffb49c193c4ecc6143c5a6af17ac
Kyle Ellrott <kellrott@gmail.com>
2020-09-23 08:49:55 -0700

    Working mutation viewer

--- bmeg_app/db/__init__.py
+++ bmeg_app/db/__init__.py
@@ -1,7 +1,38 @@
 import gripql
 import yaml
 
+import os
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search, Index
+from elasticsearch_dsl.query import MultiMatch
+
+
+
 with open('bmeg_app/config.yaml') as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
-conn = gripql.Connection("https://bmeg.io/api", credential_file = config['bmeg']['credentials'])    
+conn = gripql.Connection("https://bmeg.io/api", credential_file = config['bmeg']['credentials'])
 G = conn.graph(config['bmeg']['schema'])
+
+
+def gene_search(query):
+    """Query gene index, map to {label, value}.
+
+    Connects to elastic search via env var ELASTICSEARCH_URL"""
+
+    client = Elasticsearch(os.environ['ELASTICSEARCH_URL'])
+    search = Search(using=client, index='genes')
+    search.query = MultiMatch(query=query, type='bool_prefix', fields=[
+        'symbol',
+        'symbol._2gram',
+        'symbol._3gram',
+        'ensemble_id',
+        'ensemble_id._2gram',
+        'ensemble_id._3gram',
+        ])
+
+    # map
+    # [<Hit(genes/ENSG00000073331): {'ensemble_id': 'ENSG00000073331', 'symbol': 'ALPK1'}>]
+    # to
+    #  {"label": "New York City", "value": "NYC"},
+
+    return([{'label': f'{h.symbol}/{h.ensemble_id}', 'value': f'{h.symbol}/{h.ensemble_id}'} for h in search])
--- bmeg_app/views/mutation_view.py
+++ bmeg_app/views/mutation_view.py
@@ -1,9 +1,10 @@
 from .. import appLayout as ly
 from ..app import app
-from ..db import G
+from ..db import G, gene_search
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
+from dash.exceptions import PreventUpdate
 from dash.dependencies import Input, Output, State
 import dash_html_components as html
 import dash_table
@@ -20,26 +21,28 @@ from plotly.subplots import make_subplots
 main_colors= ly.main_colors
 styles=ly.styles
 
-cds = G.query().V("ENSG00000141510").out("alleles").render("cds_position").execute()
-o = []
-for i in cds:
-    v = i.split("/")[0]
-    try:
-        d = int(v)
-        o.append(d)
-    except ValueError:
-        pass
-df = pd.Series(o)
-counts = df.value_counts()
-mData = {
-    "x" : list("%d.0" % (i) for i in counts.index),
-    "y" : list("%d" % (i) for i in counts.values)
-}
+def getGeneMutations(gene):
+    cds = G.query().V(gene).out("alleles").render("cds_position").execute()
+    o = []
+    for i in cds:
+        v = i.split("/")[0]
+        try:
+            d = int(v)
+            o.append(d)
+        except ValueError:
+            pass
+    df = pd.Series(o)
+    counts = df.value_counts()
+    mData = {
+        "x" : list("%d.0" % (i) for i in counts.index),
+        "y" : list("%d" % (i) for i in counts.values)
+    }
+    return mData
 #print(json.dumps(mData))
 
 component = dash_bio.NeedlePlot(
   id='my-dashbio-needleplot',
-  mutationData=mData,
+  mutationData={},
   needleStyle={
         'stemColor': '#FF8888',
         'stemThickness': 2,
@@ -56,6 +59,28 @@ component = dash_bio.NeedlePlot(
 print('loading app layout')
 NAME="OncoPrint"
 tab_layout = html.Div(children=[
-    html.Label('OncoPrint HERE!!!'),
+    html.Label("Gene:"), dcc.Dropdown(id='single-dropdown'),
     component
 ])
+
+@app.callback(
+    dash.dependencies.Output('single-dropdown', 'options'),
+    [dash.dependencies.Input('single-dropdown', 'search_value')]
+)
+def update_options(search_value):
+    """Lookup the search value in elastic."""
+    if not search_value:
+        raise PreventUpdate
+    genes = gene_search(search_value)
+    return genes
+
+
+
+@app.callback(
+    dash.dependencies.Output('my-dashbio-needleplot', 'mutationData'),
+    [dash.dependencies.Input('single-dropdown', 'value')])
+def process_single(value):
+    if not value:
+        raise PreventUpdate
+    gene = value.split("/")[1]
+    return getGeneMutations(gene)
