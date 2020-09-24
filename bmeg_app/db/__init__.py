71e4e7d1a6c5ca54e9f5cb7c990783396a2dd680
Kyle Ellrott <kellrott@gmail.com>
2020-09-24 12:50:05 -0700

    Removing some old code

--- bmeg_app/db/__init__.py
+++ bmeg_app/db/__init__.py
@@ -46,12 +46,6 @@ def gene_search(query):
         'ensemble_id._2gram',
         'ensemble_id._3gram',
         ])
-
-    # map
-    # [<Hit(genes/ENSG00000073331): {'ensemble_id': 'ENSG00000073331', 'symbol': 'ALPK1'}>]
-    # to
-    #  {"label": "New York City", "value": "NYC"},
-
     return([{'label': f'{h.symbol}/{h.ensemble_id}', 'value': f'{h.symbol}/{h.ensemble_id}'} for h in search])
 
 @cache.memoize(timeout=TIMEOUT)
