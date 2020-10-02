6864a2e0d41e64fa85bcefe922a1f6b56d39d14e
Kyle Ellrott <kellrott@gmail.com>
2020-10-02 11:16:49 -0700

    Starting refactor

--- bmeg_app/views/literature_support_view.py
+++ bmeg_app/views/literature_support_view.py
@@ -1,26 +1,24 @@
 from .. import appLayout as ly
 from ..app import app
 from ..components import literature_support_component as lsu
-from ..db import G
+from ..db import G, gene_search
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
+from dash.exceptions import PreventUpdate
 from dash.dependencies import Input, Output, State
 import dash_html_components as html
 import dash_table
 import gripql
 import json
 import pandas as pd
-from plotly.subplots import make_subplots
+import plotly.express as px
 
 #######
 # Prep
 #######
 main_colors= ly.main_colors
 styles=ly.styles
-# base_df = lsu.get_baseDF() # commented out for ONLY dev to speed up loading
-# base_df.to_csv('bmeg_app/source/basedf.tsv',sep='\t',index=False) # commented out for ONLY dev to speed up loading
-base_df=pd.read_csv('bmeg_app/source/basedf.tsv',sep='\t') # TEMP TODO change to cached
 
 #######
 # Page
@@ -35,8 +33,7 @@ LAYOUT = html.Div(children=[
                     [
                         html.Label('Gene Symbol'),
                         dcc.Dropdown(id='gene_dd',
-                            options=[{'label': l, 'value': gid} for l,gid in lsu.gene_dd_selections(base_df,'geneID','gene').items()],
-                            value=[{'label': l, 'value': gid} for l,gid in lsu.gene_dd_selections(base_df,'geneID','gene').items()][0]['value'],
+                            value="MTOR/ENSG00000198793", search_value="MTOR/ENSG00000198793"
                         ),
                     ],style={'width': '100%','display': 'inline-block','font-size' : styles['t']['size_font']}
                 ),
@@ -46,90 +43,88 @@ LAYOUT = html.Div(children=[
     html.Hr(),
     dbc.Row(
         [
-            dbc.Col(dcc.Loading(id='occr', type="default",children=html.Div()),width=3,style={"c":"100%"}),
-            dbc.Col(dcc.Loading(id='resp_histo', type="default",children=html.Div()),width=4,style={"height":"100%"}),        
-            dbc.Col(dcc.Loading(id='pie_taxon', type="default",children=html.Div()),width=5,style={"height":"100%"}),     
-  
+            dbc.Col(html.Div(id='occr', children=html.Div()),width=3,style={"c":"100%"}),
+            dbc.Col(html.Div(id='resp_histo', children=html.Div()),width=4,style={"height":"100%"}),
+            dbc.Col(html.Div(id='pie_taxon', children=html.Div()),width=5,style={"height":"100%"}),
+
         ],
     ),
     html.Hr(),
-    dcc.Loading(id='evd', type="default",children=html.Div()),     
+    html.Div(id='evd', children=html.Div()),
 ],style={'fontFamily': styles['t']['type_font']})
 
 
 @app.callback(
-    Output("evd", "children"),
+    dash.dependencies.Output('gene_dd', 'options'),
+    [dash.dependencies.Input('gene_dd', 'search_value')]
+)
+def update_options(search_value):
+    """Lookup the search value in elastic."""
+    if not search_value:
+        raise PreventUpdate
+    genes = gene_search(search_value)
+    return genes
+
+@app.callback(
+    [Output("evd", "children"), Output("resp_histo", "children"),Output("pie_taxon", "children"), Output("occr", "children")],
     [Input('gene_dd', 'value')]
 )
-def render_callback(selected_gene):
+def build_evidence_table(search_value):
+    gene = search_value.split("/")[1]
     '''Occurance table'''
-    # Filter for gene
-    df = base_df[base_df['geneID']==selected_gene].reset_index(drop=True)
-    # Generate table
-    dictionary = lsu.parse_src_doc(df, 'litETC',['cancerType','drugAbstracts'])
-    df = lsu.build_publication_table(dictionary)
-    dashtable = dash_table.DataTable(
-        id='export_pub_table',
-        data = df.to_dict('records'),
-        columns=[{"name": i, "id": i} for i in df.columns],
+    mapping = {
+        "id" : "$._gid",
+        "evidence_label" : "evidence_label",
+        "oncogenic": "oncogenic",
+        "response_type" : "response_type",
+        "source" : "source"
+    }
+    g2p = []
+    response = []
+    for row in G.query().V(gene).out("g2p_associations").render(mapping):
+        g2p.append(row.to_dict())
+        response.append(row['response_type'])
+
+    '''Occurance table'''
+    evidence_table = dash_table.DataTable(
+        id='export_g2p_table',
+        data = g2p,
+        columns=[{"name": i, "id": i} for i in ["evidence_label", "oncogenic", "response_type", "source"]],
         style_header={'text-align':'center','backgroundColor': 'rgb(230, 230, 230)','fontSize':styles['t']['size_font'],'fontWeight': 'bold','fontFamily':styles['t']['type_font']},
         style_cell={'maxWidth':'100px','padding-left': '20px','padding-right': '20px'},
         style_data={'whiteSpace':'normal','height':'auto','text-align':'center','fontFamily':styles['t']['type_font'],'fontSize':styles['t']['size_font']},
         style_data_conditional=[{'if': {'row_index': 'odd'},'backgroundColor': 'rgb(248, 248, 248)'}],
         style_table={'overflow':'hidden'},
         style_as_list_view=True,
-        tooltip_data=[{column: {'value': str(value), 'type': 'markdown'} for column, value in row.items()} for row in df.to_dict('records')],
+        #tooltip_data=[{column: {'value': str(value), 'type': 'markdown'} for column, value in row.items()} for row in df.to_dict('records')],
         tooltip_duration=None,
         page_size=10,
     )
-    return dashtable,
 
-@app.callback(
-    Output("resp_histo", "children"),
-    [Input('gene_dd', 'value')]
-)
-def render_callback(selected_gene):
-    '''pie chart taxonomy'''
-    # Filter for gene
-    df = base_df[base_df['geneID']==selected_gene].reset_index(drop=True)
-    # Generate figure
-    fig= lsu.get_histogram_side(base_df['response'],main_colors['pale_yellow'])
-    return html.Div(dcc.Graph(figure=fig)),
-
-@app.callback(
-    Output("pie_taxon", "children"),
-    [Input('gene_dd', 'value')]
-)
-def render_callback(selected_gene):
-    '''pie chart taxonomy'''
-    # Filter for gene
-    df = base_df[base_df['geneID']==selected_gene].reset_index(drop=True)
-    # Generate figure
-    fig= lsu.pie_from_dict(lsu.count_taxonomy(df),False)
-    return html.Div(dcc.Graph(figure=fig)),
-
-@app.callback(
-    Output("occr", "children"),
-    [Input('gene_dd', 'value')]
-)
-def render_callback(selected_gene):
-    '''Occurance table'''
-    # Filter for gene
-    df = base_df[base_df['geneID']==selected_gene].reset_index(drop=True)
-    # Generate table
-    df = lsu.occurances_table(df,'drug',['Compound','Lit Occurances'])
-    dashtable = dash_table.DataTable(
+    pub_table = dash_table.DataTable(
         id='export_pub_table',
-        data = df.to_dict('records'),
-        columns=[{"name": i, "id": i} for i in df.columns],
+        data = g2p,
+        columns=[{"name": i, "id": i} for i in ["evidence_label", "oncogenic", "response_type", "source"]],
         style_header={'text-align':'center','backgroundColor': 'rgb(230, 230, 230)','fontSize':styles['t']['size_font'],'fontWeight': 'bold','fontFamily':styles['t']['type_font']},
         style_cell={'maxWidth':'100px','padding-left': '20px','padding-right': '20px'},
         style_data={'whiteSpace':'normal','height':'auto','text-align':'center','fontFamily':styles['t']['type_font'],'fontSize':styles['t']['size_font']},
         style_data_conditional=[{'if': {'row_index': 'odd'},'backgroundColor': 'rgb(248, 248, 248)'}],
         style_table={'overflow':'hidden'},
         style_as_list_view=True,
-        tooltip_data=[{column: {'value': str(value), 'type': 'markdown'} for column, value in row.items()} for row in df.to_dict('records')],
+        #tooltip_data=[{column: {'value': str(value), 'type': 'markdown'} for column, value in row.items()} for row in df.to_dict('records')],
         tooltip_duration=None,
         page_size=10,
     )
-    return dashtable,
+
+    #fig= px.histogram(response, orientation="h")
+    #histogram = html.Div(dcc.Graph(figure=fig)),
+    histogram = html.Div(dcc.Graph())
+
+    '''pie chart compound taxonomy'''
+    associated_compounds = G.query().V(gene).out("g2p_associations").as_("a").out("compounds").as_("c").render(["$a._gid", "$c._gid"]).execute()
+
+    c = [] #G.query().V(list(pd.DataFrame(associated_compounds)[1].unique())).render(["_gid", "taxonomy.superclass"]).execute()
+    compound_info = pd.DataFrame(c, columns=["id", "superclass"]).set_index("id")
+    pie_chart = dcc.Graph(figure=px.pie(compound_info, names="superclass"))
+
+    return html.Div(pub_table), histogram, pie_chart, html.Div(evidence_table)
