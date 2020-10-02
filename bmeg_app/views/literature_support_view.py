7160ed18ff23aa6cdf58cbf85e17290080c888c2
Kyle Ellrott <kellrott@gmail.com>
2020-10-02 15:13:01 -0700

    Getting the G2P figures to work again

--- bmeg_app/views/literature_support_view.py
+++ bmeg_app/views/literature_support_view.py
@@ -45,14 +45,14 @@ LAYOUT = html.Div(children=[
     html.Hr(),
     dbc.Row(
         [
-            dbc.Col(html.Div(id='occr', children=html.Div()),width=3,style={"c":"100%"}),
-            dbc.Col(html.Div(id='resp_histo', children=html.Div()),width=4,style={"height":"100%"}),
-            dbc.Col(html.Div(id='pie_taxon', children=html.Div()),width=5,style={"height":"100%"}),
+            dbc.Col(dcc.Loading(id='occr', children=html.Div()),width=3,style={"c":"100%"}),
+            dbc.Col(dcc.Loading(id='resp_histo', children=html.Div()),width=4,style={"height":"100%"}),
+            dbc.Col(dcc.Loading(id='pie_taxon', children=html.Div()),width=5,style={"height":"100%"}),
 
         ],
     ),
     html.Hr(),
-    html.Div(id='evd', children=html.Div()),
+    dcc.Loading(id='evd', children=html.Div()),
 ],style={'fontFamily': styles['t']['type_font']})
 
 
@@ -94,7 +94,7 @@ def build_evidence_table(search_value):
         columns=[{"name": i, "id": i} for i in ["evidence_label", "oncogenic", "response_type", "source"]],
         style_header={'text-align':'center','backgroundColor': 'rgb(230, 230, 230)','fontSize':styles['t']['size_font'],'fontWeight': 'bold','fontFamily':styles['t']['type_font']},
         style_cell={'maxWidth':'100px','padding-left': '20px','padding-right': '20px'},
-        style_data={'whiteSpace':'normal','height':'auto','text-align':'center','fontFamily':styles['t']['type_font'],'fontSize':styles['t']['size_font']},
+        style_data={'whiteSpace':'normal','height':'auto','text-align':'left','fontFamily':styles['t']['type_font'],'fontSize':styles['t']['size_font']},
         style_data_conditional=[{'if': {'row_index': 'odd'},'backgroundColor': 'rgb(248, 248, 248)'}],
         style_table={'overflow':'hidden'},
         style_as_list_view=True,
@@ -102,31 +102,43 @@ def build_evidence_table(search_value):
         tooltip_duration=None,
         page_size=10,
     )
+
+    citations = G.query().V(gene).out("g2p_associations").as_("a").out("publications").as_("p").select("a").out("compounds").as_("c").aggregate(gripql.term("chem_citation", "$c.synonym")).execute()
+    citation_counts = []
+    for i in citations[0]["chem_citation"]["buckets"]:
+        citation_counts.append( { "compound" : i['key'], "citations" : i['value'] } )
 
     pub_table = dash_table.DataTable(
         id='export_pub_table',
-        data = g2p,
-        columns=[{"name": i, "id": i} for i in ["evidence_label", "oncogenic", "response_type", "source"]],
-        style_header={'text-align':'center','backgroundColor': 'rgb(230, 230, 230)','fontSize':styles['t']['size_font'],'fontWeight': 'bold','fontFamily':styles['t']['type_font']},
+        data = citation_counts[:10],
+        columns=[{"name": "Compound", "id": "compound"}, {"name" : "Citations", "id" : "citations"}],
+        style_header={'text-align':'left','backgroundColor': 'rgb(230, 230, 230)','fontSize':styles['t']['size_font'],'fontWeight': 'bold','fontFamily':styles['t']['type_font']},
         style_cell={'maxWidth':'100px','padding-left': '20px','padding-right': '20px'},
-        style_data={'whiteSpace':'normal','height':'auto','text-align':'center','fontFamily':styles['t']['type_font'],'fontSize':styles['t']['size_font']},
+        style_data={'whiteSpace':'normal','height':'auto','text-align':'left','fontFamily':styles['t']['type_font'],'fontSize':styles['t']['size_font']-2},
         style_data_conditional=[{'if': {'row_index': 'odd'},'backgroundColor': 'rgb(248, 248, 248)'}],
         style_table={'overflow':'hidden'},
-        style_as_list_view=True,
-        #tooltip_data=[{column: {'value': str(value), 'type': 'markdown'} for column, value in row.items()} for row in df.to_dict('records')],
+        style_as_list_view=True,        #tooltip_data=[{column: {'value': str(value), 'type': 'markdown'} for column, value in row.items()} for row in df.to_dict('records')],
         tooltip_duration=None,
         page_size=10,
     )
 
-    #fig= px.histogram(response, orientation="h")
-    #histogram = html.Div(dcc.Graph(figure=fig)),
-    histogram = html.Div(dcc.Graph())
+    height = 250
+
+    fig= px.histogram(response, orientation="h", height=height, title="Response")
+    fig.update_layout(margin={'t':25, 'b':0, 'r': 0},
+            yaxis=dict(tickmode='linear'),
+            plot_bgcolor='white',
+            paper_bgcolor='white')
+    fig.update_layout(showlegend=False)
+    histogram = dcc.Graph(figure=fig)
 
     '''pie chart compound taxonomy'''
     associated_compounds = G.query().V(gene).out("g2p_associations").as_("a").out("compounds").as_("c").render(["$a._gid", "$c._gid"]).execute()
-
-    c = [] #G.query().V(list(pd.DataFrame(associated_compounds)[1].unique())).render(["_gid", "taxonomy.superclass"]).execute()
+    c = G.query().V(list(pd.DataFrame(associated_compounds)[1].unique())).render(["_gid", "taxonomy.superclass"]).execute()
     compound_info = pd.DataFrame(c, columns=["id", "superclass"]).set_index("id")
-    pie_chart = dcc.Graph(figure=px.pie(compound_info, names="superclass"))
+    fig = px.pie(compound_info, names="superclass", height=height, title="Compound Superclass")
+    fig.update_layout(showlegend=False, margin={'t':25, 'b':0, 'l':0, 'r': 0})
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+    pie_chart = dcc.Graph(figure=fig)
 
-    return html.Div(pub_table), histogram, pie_chart, html.Div(evidence_table)
+    return evidence_table, histogram, pie_chart, pub_table
