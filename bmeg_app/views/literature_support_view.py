bcd49575b8dbdee822077ebce2dd6fdd62fb026a
Jordan Lee <leejor@ohsu.edu>
2020-10-05 17:22:10 -0700

    clean up visuals

--- bmeg_app/views/literature_support_view.py
+++ bmeg_app/views/literature_support_view.py
@@ -1,6 +1,5 @@
 from .. import appLayout as ly
 from ..app import app
-from ..components import literature_support_component as lsu
 from ..db import G, gene_search
 import dash
 import dash_bootstrap_components as dbc
@@ -47,8 +46,8 @@ LAYOUT = html.Div(children=[
     dbc.Row(
         [
             dbc.Col(dcc.Loading(id='occr', children=html.Div()),width=3,style={"height":"100%"}),
-            dbc.Col(dcc.Loading(id='resp_histo', children=html.Div()),width=4,style={"height":"100%"}),
-            dbc.Col(dcc.Loading(id='pie_taxon', children=html.Div()),width=5,style={"height":"100%"}),
+            dbc.Col(dcc.Loading(id='resp_histo', children=html.Div()),width=3,style={"height":"100%"}),
+            dbc.Col(dcc.Loading(id='pie_taxon', children=html.Div()),width=6,style={"height":"100%"}),
 
         ],
     ),
@@ -88,8 +87,6 @@ def build_evidence_table(search_value):
     for row in G.query().V(gene).out("g2p_associations").render(mapping):
         g2p.append(row.to_dict())
         response.append(row['response_type'])
-    print('this is response ', len(response))
-    print(response)
     # If no responses found exit
     if len(response)==0:
         return html.Label('No Responses Found'),html.Label('No Responses Found'),html.Label('No Responses Found'),html.Label('No Responses Found')
@@ -123,7 +120,6 @@ def build_evidence_table(search_value):
         {"name": "Title", "id": "title", 'presentation':'markdown'},
         {"name": "Compounds", "id": "compound"},
         {"name": "Source", "id": "source"},
-        {"name": "Oncogenic", "id": "oncogenic"},
         {"name": "Response", "id": "response_type"},
         {"name": "Evidence Level", "id": "evidence_label"}
     ]
@@ -134,7 +130,7 @@ def build_evidence_table(search_value):
         columns=table_columns,
         style_header={'text-align':'center','backgroundColor': 'rgb(230, 230, 230)','fontSize':styles['t']['size_font'],'fontWeight': 'bold','fontFamily':styles['t']['type_font']},
         style_cell={'maxWidth':'100px','padding-left': '20px','padding-right': '20px'},
-        style_data={'whiteSpace':'normal','height':'auto','text-align':'left','fontFamily':styles['t']['type_font'],'fontSize':styles['t']['size_font']},
+        style_data={'whiteSpace':'normal','height':'auto','text-align':'center','fontFamily':styles['t']['type_font'],'fontSize':styles['t']['size_font']},
         style_data_conditional=[{'if': {'row_index': 'odd'},'backgroundColor': 'rgb(248, 248, 248)'}],
         style_table={'overflow':'hidden'},
         style_as_list_view=True,
@@ -152,9 +148,9 @@ def build_evidence_table(search_value):
         id='export_pub_table',
         data = citation_counts[:10],
         columns=[{"name": "Top 10 Compounds", "id": "compound"}, {"name" : "Citations", "id" : "citations"}],
-        style_header={'text-align':'left','backgroundColor': 'rgb(230, 230, 230)','fontSize':styles['t']['size_font'],'fontWeight': 'bold','fontFamily':styles['t']['type_font']},
+        style_header={'text-align':'center','backgroundColor': 'rgb(230, 230, 230)','fontSize':styles['t']['size_font'],'fontWeight': 'bold','fontFamily':styles['t']['type_font']},
         style_cell={'maxWidth':'100px','padding-left': '20px','padding-right': '20px'},
-        style_data={'whiteSpace':'normal','height':'auto','text-align':'left','fontFamily':styles['t']['type_font'],'fontSize':styles['t']['size_font']-2},
+        style_data={'whiteSpace':'normal','height':'auto','text-align':'center','fontFamily':styles['t']['type_font'],'fontSize':styles['t']['size_font']-2},
         style_data_conditional=[{'if': {'row_index': 'odd'},'backgroundColor': 'rgb(248, 248, 248)'}],
         style_table={'overflow':'hidden'},
         style_as_list_view=True,        #tooltip_data=[{column: {'value': str(value), 'type': 'markdown'} for column, value in row.items()} for row in df.to_dict('records')],
@@ -162,17 +158,28 @@ def build_evidence_table(search_value):
         page_size=10,
     )
 
-    height = 300
-    fig = lsu.get_histogram_side(response, main_colors['pale_orange'], 'Response', '', '')
+    height = 310
+
+    fig= px.histogram(response, orientation="h", height=height, title="Cell Line Response",labels={'count':'Count', 'value':''})
+    fig.update_layout(margin={'t':25, 'b':0, 'r': 0},
+        yaxis=dict(tickmode='linear'),
+        plot_bgcolor='white',
+        paper_bgcolor='white',
+        showlegend=False)
     histogram = dcc.Graph(figure=fig)
 
     '''pie chart compound taxonomy'''
     associated_compounds = G.query().V(gene).out("g2p_associations").as_("a").out("compounds").as_("c").render(["$a._gid", "$c._gid"]).execute()
     c = G.query().V(list(pd.DataFrame(associated_compounds)[1].unique())).render(["_gid", "taxonomy.superclass"]).execute()
     compound_info = pd.DataFrame(c, columns=["id", "superclass"]).set_index("id")
-    fig = px.pie(compound_info, names="superclass", height=height, title="Compound Superclass")
-    fig.update_layout(showlegend=False, margin={'t':25, 'b':0, 'l':0, 'r': 0})
-    fig.update_traces(textposition='inside', textinfo='percent+label')
+
+    # Remove NA superclasses
+    subset= compound_info.dropna()
+    na_dropped= len(compound_info.index)-len(subset.index)
+
+    fig = px.pie(subset, names="superclass", height=height, title="Compound Superclass ({} without superclass info)".format(na_dropped))
+    fig.update_layout(showlegend=False, margin={'t':25, 'b':0})
+    fig.update_traces(textinfo='label')
     pie_chart = dcc.Graph(figure=fig)
 
     return evidence_table, histogram, pie_chart, pub_table
