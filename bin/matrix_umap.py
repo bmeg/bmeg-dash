81278d1bedee13683d134b8b81739d1f0ec18109
Kyle Ellrott <kellrott@gmail.com>
2020-10-07 13:09:10 -0700

    Setting up snakemake based build for umap coordinate calculation

---
+++ bin/matrix_umap.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+import pandas as pd
+import umap
+
+def umap_tsv(path):
+    df = pd.read_csv(path, sep="\t", index_col=0)
+    locs = umap.UMAP().fit_transform(df)
+    return pd.DataFrame(locs, index=df.index)
+
+inPath = sys.argv[1]
+outPath = sys.argv[2]
+
+locFrame = umap_tsv(inPath)
+locFrame.to_csv(outPath, sep="\t")
